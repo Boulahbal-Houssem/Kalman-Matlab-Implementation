7260afb4c0f4254ad0b1b1e40a1c2f4cd07e910c
Boulahbal-Houssem <boulahbalhoussem@gmail.com>
2019-04-20 19:08:53 +0200

    Kalman's filter updated

--- kalman.m
+++ kalman.m
@@ -1,58 +1,75 @@
-function [ output_args ] = kalman( N,T,Z,F,Hfull,mX0,PX0,Qw,Rv,X )
-% UNTITLED2 Summary of this function goes here
-% Detailed explanation goes here
-N=50;
+function [ Kalmantable, XE ] = kalman( N,T,Z,F,mX0,PX0,Qw,Rv ,H1 ,H2, Hfull)
+% This function Implements linear kalman filter
+%   @inputs : 
+%		N         : numbre of time samples
+%		T         : vector of samples (1xN)
+%		Z         : postions measurement of robot and amers
+%		F 	  : dynamic matrices that describe the système
+%		mX0       : expectation of the inital postion		
+%		PX0       : expectation of the inital covarience of the postion
+%		Qw        : dynamic noise covarience 
+%		Rv        : measurement noise covarience
+%		H1        : Dynamic when sensor 1 is activated
+%		H2        : Dynamic when sensor 2 is activated
+%		Hfull     : Dynamic when both sensors are activated
+%   @Outputs :		
+%		Kalmantable(1,N) : Predicated postion	
+%		Kalmantable(2,N) : Predicated covarience of postion	
+%		Kalmantable(3,N) : Updated state estimate	
+%		Kalmantable(4,N) : Updated covarience estimate	
+%		Kalmantable(5,N) : Kalman's gain 	
+		
+deltaT = 1;
 W = chol(Qw)'*randn(6,N);
 Kalmantable = cell(5,N);
-
-H1=[-1 0 1 0 0 0; 0 -1 0 1 0 0];
-H2=[-1 0 0 0 1 0 ; 0 -1 0 0 0 1];
-H=[H1;H2]; Hfull=H;
-F = [cos(w*deltaT) -sin(w*deltaT) ; sin(w*deltaT) cos(w*deltaT)];
 Source1Vue=zeros(1,N);
 Source2Vue=zeros(1,N);
 Kalmantable(1,1) ={nan*ones(6,1)};
 Kalmantable(2,1) ={nan*ones(6,6)};
 Kalmantable(3,1) ={mX0};
 Kalmantable(4,1) ={PX0};
-Kalmantable(5,1) ={nan}
+Kalmantable(5,1) ={nan};
 for i=2:N
-   % Prediction    
-   Kalmantable(1,i)= F * Kalmantable(3,i-1); % Xpred iter i
-   Kalmantable(2,i)= F * Kalmantable(4,i-1) *F' + Qw ;%Ppred iter i
-   % Mise a jour du postion 
-   % cheking Amers visibility
-   if (~isnan(Z(1,i)))
-     Source1Vue(k)=1;
-  end
-   if (~isnan(Z(3,i)))
-     Source2Vue(k)=1;
-   end 
-   % Calculating gain
-   if (Source1Vue(k)==1)&&(Source2Vue(k)==1)
-    gain = Kalmantable(2,i) * Hfull' * inv(Rv + Hfull * Kalmantable(2,i) * Hfull')
-    Kalmantable(3,i)= Kalmantable(1,i) + gain * ( Z(:,i) - Hfull * X(i) );
-    Kalmantable(4,i)= Kalmantable(2,i) - gain * ( Z(:,i) - Hfull * X(i) );
-
-  elseif (Source1Vue(k)==1)&&(Source2Vue(k)==0)
-      gain = Kalmantable(2,i) * H1' * inv(Rv + H1 * Kalmantable(2,i) * H1')
-  elseif (Source1Vue(k)==0)&&(Source2Vue(k)==1)
-      gain = Kalmantable(2,i) * H2' * inv(Rv + H2 * Kalmantable(2,i) * H2')
-   else
-      gain = nan;
-      
-   end
-   Kalmantable(5,i)=gain;
-    % update variables
-       
-       
-  end
-   
-   
-   
+	% Prediction
+	Xest = cell2mat(Kalmantable(3,i-1))
+	Pest = cell2mat(Kalmantable(4,i-1))
+	Xpred = F * Xest;
+	Ppred = F * Pest * F' + Qw;
+	Kalmantable(1,i)= F * Kalmantable(3,i-1); % Xpred iter i
+	Kalmantable(2,i)= F * Kalmantable(4,i-1) *F' + Qw ;%Ppred iter i
+	% Mise a jour du postion
+	% cheking Amers visibility
+	if (~isnan(Z(1,i)))
+		Source1Vue(i)=1;
+	end
+	if (~isnan(Z(3,i)))
+		Source2Vue(i)=1;
+	end
+	% Calculating gain
+	if (Source1Vue(i)==1)&&(Source2Vue(i)==1)
+		H = Hfull;
+		R =Rv;
+		z= Z(:,i);
+	elseif (Source1Vue(i)==1)&&(Source2Vue(i)==0)
+		H = H1;
+		R =Rv(1:2,1:2);
+		z= Z(1:2,i);
+	elseif (Source1Vue(i)==0)&&(Source2Vue(i)==1)
+	H = H2;
+	R =Rv(3:4,3:4)
+	z= Z(3:4,i);
+	else
+	H = zeros([4 6]);
+	R =Rv;
+	z =zeros([4 1]);
+	gain=Ppred * H' * inv(R + H * Ppred * H');
+	% update variables
+	Kalmantable(1,i)= {Xpred};
+	Kalmantable(2,i)= {Ppred};
+	Kalmantable(3,i)= {Xpred + gain * ( z - H *Xpred)}
+	Kalmantable(4,i)= {Ppred - gain * H * Ppred };
+	Kalmantable(5,i) = {gain};
+	%XE(i,:) = Xpred + gain * ( z - H *Xpred);
+	XE(i,:) = Xpred
+	
 end
-    
-    
-    
-end
-
--- main.m
+++ main.m
@@ -1,1 +1,21 @@
-[N,T,Z,F,Hfull,mX0,PX0,Qw,Rv,X] = simulationDonnees(1)
+[N,T,Z,F,Hfull,mX0,PX0,Qw,Rv,X] = simulationDonnees(1);
+[d s] = kalman(N,T,Z,F,Hfull,mX0,PX0,Qw,Rv,X);
+subplot(1,2,1)
+plot(s(2:50,1),s(2:50,2))
+hold on
+plot(s(2:50,3),s(2:50,4),'r')
+hold on
+plot(s(2:50,5),s(2:50,6),'y')
+hold off
+xlim([-5 5])
+ylim([-5 5])
+title('X estimé avec filtre de Kalman')
+subplot(1,2,2)
+plot(X(1,2:50),X(2,2:50))
+hold on
+plot(X(3,2:50),X(4,2:50),'r')
+hold on
+plot(X(5,2:50),X(6,2:50),'y')
+title('X sans filtre de Kalman')
+xlim([-5 5])	
+ylim([-5 5])
